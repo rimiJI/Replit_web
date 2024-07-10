12771aede39c6117c1b9b5bec23ca14aafbc6d30
rimiJI <94333495+rimiJI@users.noreply.github.com>
2024-07-10 23:56:39 +0000

    nomadcoder_challenge_Day4

---
+++ caculater.py
@@ -0,0 +1,35 @@
+playing = True
+
+
+a = int(input("Choose a number:\n"))
+b = int(input("Choose another one:\n"))
+operation = input(
+    "Choose an operation:\n    Options are: + , - , * or /.\n    Write 'exit' to finish.\n" )
+
+
+while playing:
+    a = int(input("Choose a number:\n"))
+    b = int(input("Choose another one:\n"))
+    operation = input(
+        "Choose an operation:\n    Options are: + , - , * or /.\n    Write 'exit' to finish.\n" )
+    
+    if operation == "+":
+        print(f"Result: {a + b}")
+        print("------------")
+    elif operation == "-":
+        print(f"Result: {a - b}")
+        print("------------")
+    elif operation == "*":
+        print(f"Result: {a*b}")
+        print("------------")
+    elif operation == "/":
+        print(f"Result: {a/b}")
+        print("------------")
+    elif operation == "exit":
+        playing = False
+        break
+    else:
+        print("You wrote wrong operation. Please try again.")
+        
+
+
